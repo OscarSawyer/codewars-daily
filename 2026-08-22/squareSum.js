1dc57b95d2dfc4276862ac510eb83ce55282ea16
Oscar Nyegyema <nyegyema2019@gmail.com>
2026-08-22 00:22:33 +0300

    solved a 7kyu challenge for squaring all numbers passed into a function and returning the sum of the squares

---
+++ 2026-08-22/squareSum.js
@@ -0,0 +1,12 @@
+
+// Complete the square sum function so that it squares each number 
+// passed into it and then sums the results together.
+
+
+function squareSum(numbers){
+let sum = 0;
+  numbers.forEach(num =>{
+    sum += num*num
+  })
+  return sum
+}
